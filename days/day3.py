857673d35b2e207691d06ca0a390a8a48f25e6d9
Sam Gonshaw <samgonshaw@gmail.com>
2025-12-03 07:20:24 +0000

    day3

---
+++ days/day3.py
@@ -0,0 +1,27 @@
+
+import operator
+
+def find_batteries(lines, ons=2):
+    """
+    Find the battery choice for a given number of battery turn ons, and sum
+    """
+    sm = 0
+    for l in lines:
+        l=l.strip()
+        res = 0
+        maxidx = -1
+        for rem in range(ons - 1, -1, -1):
+            if rem > 0:
+                maxidx, maxv = max(enumerate(l[maxidx + 1:-rem], start=maxidx+1), key=operator.itemgetter(1))
+            else:
+                maxidx, maxv = max(enumerate(l[maxidx + 1:], start=maxidx+1), key=operator.itemgetter(1))
+            res = res * 10 + int(maxv)
+        sm += res
+    return sm
+
+
+def run(fs):
+    p1 = find_batteries(fs, 2)
+    fs.seek(0)
+    p2 = find_batteries(fs, 12)
+    return p1,p2
---
+++ test/test_day3.py
@@ -0,0 +1,14 @@
+from ..days.day3 import find_batteries
+
+
+def test_part1_2():
+    l = [
+        "987654321111111",
+        "811111111111119",
+        "234234234234278",
+        "818181911112111",
+    ]
+    res = find_batteries(l,2)
+    assert res == 357
+    res = find_batteries(l,12)
+    assert res == 3121910778619
